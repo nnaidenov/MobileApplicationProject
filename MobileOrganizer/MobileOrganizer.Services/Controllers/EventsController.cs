8ccb8e594d3eca443549b8a1a1f067fef6a1aad8
nnaidenov <n.naidenov92@gmail.com>
2013-10-13 02:45:52 +0300

    add getAll eventsaction

--- MobileOrganizer/MobileOrganizer.Services/Controllers/EventsController.cs
+++ MobileOrganizer/MobileOrganizer.Services/Controllers/EventsController.cs
@@ -47,5 +47,37 @@ namespace MobileOrganizer.Services.Controllers
 
             return responseMsg;
         }
+
+        [HttpGet]
+        [ActionName("all")]
+        public ICollection<StuffListModel> All(int day, int mounth, int year,
+             [ValueProvider(typeof(HeaderValueProviderFactory<string>))] string sessionKey)
+        {
+            var responseMsg = ExecuteOperationOrHandleExceptions(
+            () =>
+            {
+                var user = this.Data.Users.FirstOrDefault(u => u.SessionKey == sessionKey);
+                if (user == null)
+                {
+                    throw new InvalidOperationException("Invalid username or password");
+                }
+                DateTime searchDay = new DateTime(year, mounth, day);
+
+                var events = this.Data.Events.Where(t => t.OwnerId == user.Id && t.StartDate >= searchDay).OrderBy(t => t.Priority);
+
+                var modelsEvents =
+                    (from e in events
+                     select new StuffListModel
+                     {
+                         Id = e.Id,
+                         Title = e.Title,
+                         Type = "event"
+                     });
+
+                return modelsEvents.ToList();
+            });
+
+            return responseMsg;
+        }
     }
 }
