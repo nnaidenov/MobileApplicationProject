07ba293c824b17ac47c8b9d334903a96772be24f
nnaidenov <n.naidenov92@gmail.com>
2013-10-12 18:35:39 +0300

    Add StuffesController

---
+++ MobileOrganizer/MobileOrganizer.Services/Controllers/StuffesController.cs
@@ -0,0 +1,70 @@
+﻿using MobileOrganizer.Services.Attributes;
+using MobileOrganizer.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ValueProviders;
+
+namespace MobileOrganizer.Services.Controllers
+{
+    public class StuffesController : BaseApiController
+    {
+        [HttpGet]
+        [ActionName("all")]
+        public ICollection<StuffModel> All(
+             [ValueProvider(typeof(HeaderValueProviderFactory<string>))] string sessionKey)
+        {
+            var responseMsg = ExecuteOperationOrHandleExceptions(
+            () =>
+            {
+                var user = this.Data.Users.FirstOrDefault(u => u.SessionKey == sessionKey);
+                if (user == null)
+                {
+                    throw new InvalidOperationException("Invalid username or password");
+                }
+
+                var todoes = this.Data.Todos.Where(t => t.OwnerId == user.Id && t.Date >= DateTime.Now).OrderBy(t => t.Date);
+                var events = this.Data.Events.Where(t => t.OwnerId == user.Id && t.StartDate >= DateTime.Now).OrderBy(t => t.StartDate);
+
+                var modelsTodo =
+                    (from t in todoes
+                     select new StuffModel
+                     {
+                         Day = t.Date.Day,
+                         Mount = t.Date.Month - DateTime.Now.Month,
+                         Year = t.Date.Year - DateTime.Now.Year,
+                         Type = "todo"
+                     });
+
+                var modelsEvents =
+                    (from e in events
+                     select new StuffModel
+                     {
+                         Day = e.StartDate.Day,
+                         Mount = e.StartDate.Month - DateTime.Now.Month,
+                         Year = e.StartDate.Year - DateTime.Now.Year,
+                         Type = "event"
+                     });
+
+                var stuffes = new List<StuffModel>();
+
+                foreach (var todo in modelsTodo)
+                {
+                    stuffes.Add(todo);
+                }
+
+                foreach (var eventModel in modelsEvents)
+                {
+                    stuffes.Add(eventModel);
+                }
+
+                return stuffes.OrderBy(s=>s.Year).ThenBy(s=>s.Mount).ThenBy(s=>s.Day).ToList();
+            });
+
+            return responseMsg;
+        }
+    }
+}
---
+++ MobileOrganizer/MobileOrganizer.Services/Models/StuffModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileOrganizer.Services.Models
+{
+    public class StuffModel
+    {
+        public int Day { get; set; }
+        public int Mount { get; set; }
+        public int Year { get; set; }
+        public string Type { get; set; }
+    }
+}
